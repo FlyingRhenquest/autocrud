97df756954b7db6c79b5267179de6c0121a1c9ce
Bruce Ide <FlyingRhenquest@gmail.com>
2026-02-26 06:19:17 -0700

    Add some shorter helpers for annotations

---
+++ include/fr/autocrud/Helpers.h
@@ -0,0 +1,65 @@
+/**
+ * Copyright 2026 Bruce Ide
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+/**
+ * Some non-namespaced helpers for shorter annotations
+ */
+
+#include <algorithm>
+#include <fr/autocrud/Crud.h>
+#include <meta>
+#include <string_view>
+
+// Ignore maps to fr::autocrud::DbIgnore
+
+consteval auto Ignore() {
+  return fr::autocrud::DbIgnore{};
+}
+
+// TODO: Replace with std::basic_fixed_string if P3094R0 ever
+// gets implemented
+
+namespace fr::autocrud {
+
+  template <std::size_t N>
+  struct FixedString {
+    char data[N];
+    constexpr FixedString(const char (&str)[N]) {
+      std::copy_n(str, N, data);
+    }
+  };
+  
+}
+
+/**
+ * Map DbFieldType to _ColumnType maybe?
+ */
+
+template <fr::autocrud::FixedString T>
+constexpr fr::autocrud::DbFieldType operator""_ColumnType() {
+  return fr::autocrud::DbFieldType(std::define_static_string(T.data));
+}
+
+/**
+ * And DbFieldName to _ColumnName
+ */
+
+template <fr::autocrud::FixedString T>
+constexpr fr::autocrud::DbFieldName operator""_ColumnName() {
+  return fr::autocrud::DbFieldName(std::define_static_string(T.data));
+}
---
+++ test/HelpersTest.cpp
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2026 Bruce Ide
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <gtest/gtest.h>
+#include <fr/autocrud/Crud.h>
+#include <fr/autocrud/Helpers.h>
+#include <string.h>
+
+TEST(Helpers, annotations) {
+
+  struct Derived : public fr::autocrud::Node {
+    [[=Ignore() ]] int ignored;
+    [[="VARCHAR(100)"_ColumnType]] std::string foo;
+    [[="quux"_ColumnName]] std::string bar; // will be named quux
+  };
+
+  fr::autocrud::Crud<Derived> crud;
+
+  {
+    const auto [cppName, dbName, dbType, ptr] = crud.column<0>();
+    // "ignored" ignored successfully
+    ASSERT_EQ(0, strlen(cppName));
+  }
+  {
+    const auto [cppName, dbName, dbType, ptr] = crud.column<1>();
+    ASSERT_EQ("foo", std::string(cppName));
+    ASSERT_EQ("foo", std::string(dbName));
+    ASSERT_EQ("VARCHAR(100)", std::string(dbType));
+  }
+  {
+    const auto [cppName, dbName, dbType, ptr] = crud.column<2>();
+    ASSERT_EQ("bar", std::string(cppName));
+    ASSERT_EQ("quux", std::string(dbName));
+    ASSERT_EQ("TEXT", std::string(dbType));
+  }
+  
+}
